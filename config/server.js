73b5302661a0d5bd21f9b04c5dddaadc0ecb7c5d
ramprits <ramprits-sdei@gmail.com>
2021-01-12 16:07:27 +0530

    adding production

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://65cf2d9262e8.ngrok.io",
+  url:
+    process.env.NODE_ENV === "production"
+      ? "https://myapp-sahani.herokuapp.com"
+      : "http://65cf2d9262e8.ngrok.io",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "ffe9d92e64fac5970a495fd6a08604c7"),
