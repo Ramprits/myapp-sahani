ebad0482b4be05416559d175a11ac5a2d4608f2f
ramprits <ramprits-sdei@gmail.com>
2021-01-12 15:30:20 +0530

    adding production database

--- config/database.js
+++ config/database.js
@@ -1,15 +1,34 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "mongoose",
+          settings: {
+            uri: env("DATABASE_URI"),
+          },
+          options: {
+            ssl: true,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
+    };
+  } else {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "sqlite",
+            filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-    },
-  },
-});
+    };
+  }
+};
--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://65cf2d9262e8.ngrok.io",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ffe9d92e64fac5970a495fd6a08604c7'),
+      secret: env("ADMIN_JWT_SECRET", "ffe9d92e64fac5970a495fd6a08604c7"),
     },
   },
 });
